569ded7d09344cb978418c926adf538e954911cc
Mitchell C Kuppinger <dpneumo@gmail.com>
2017-12-20 18:03:05 -0600

    Move the patterns class to it's own file

--- lib/yt/parser.rb
+++ lib/yt/parser.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require_relative 'patterns'
 module Yt
   class YTUrlParser
     def parse(text)
@@ -19,36 +20,4 @@ module Yt
       m.named_captures.merge(kind: kind)
     end
   end
-
-  class Patterns
-    def unfolded
-      tagged_patterns.flat_map {|patterns| unfold(patterns) }
-    end
-
-    private
-    def tagged_patterns
-      { video:    [ %r{youtube\.com/watch\?v=(?<id>[\w-]{11})},
-                    %r{youtu\.be/(?<id>[\w-]{11})},
-                    %r{youtube\.com/embed/(?<id>[\w-]{11})},
-                    %r{youtube\.com/v/(?<id>[\w-]{11})},
-                  ].freeze,
-
-        playlist: [ %r{youtube\.com/playlist/?\?list=(?<id>[\w-]+)},
-                  ].freeze,
-
-        channel:  [ %r{youtube\.com/channel/(?<id>UC[\w-]{22})},
-                    %r{youtube\.com/(?<format>c/|user/)?(?<name>[\w-]+)},
-                  ].freeze,
-
-        unknown:  [ %r{.*} ].freeze
-      }.freeze
-    end
-
-    def unfold(patterns)
-      kind = patterns[0]
-      regexps = patterns[1]
-      regexps.map {|regex| [kind, regex] }
-    end
-  end
-
 end
