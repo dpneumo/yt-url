1878e1aa52e769d512cf52676b09c068f1d529d8
Mitchell C Kuppinger <dpneumo@gmail.com>
2017-12-21 01:46:10 -0600

    Naming improvement in resource_id_test. Add test to demonstrates need for YT API key.

--- test/resource_id_test.rb
+++ test/resource_id_test.rb
@@ -6,6 +6,12 @@ class YTResourceIdTest < MiniTest::Test
   include ResourceIdInterfaceTest
 
   def setup
-    @res_id = @object = Yt::YTResourceId.new('format' => :channel, 'name' => 'abcd123')
+    @rsrc_id = @object = Yt::YTResourceId.new('format' => :channel, 'name' => 'abcd123')
+  end
+
+  def test_resource_id_requires_an_internet_connection_to_the_youtube_api
+    assert_raises OpenSSL::SSL::SSLError do
+      @rsrc_id.fetch_id
+    end
   end
 end
